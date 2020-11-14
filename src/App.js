e9a7ee0627e4c2e2280b27f0915c02d4c7d373b5
ruida shen <shenruida@gmail.com>
2020-11-14 15:17:57 -0800

    Removed Button sample

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
-import logo from "./logo.svg";
 import "./App.css";
-import Button from "./Button";
 function App() {
-  return (
-    <div>
-      <Button label="Click me" />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
