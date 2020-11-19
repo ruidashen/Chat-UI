3c60045770057b4ddc635c648eb9d39fd0fdf3e1
ruida shen <shenruida@gmail.com>
2020-11-18 23:16:45 -0800

    InputText sub component

---
+++ src/components/Input/InputText/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import PropTypes from "prop-types";
+import StyledInputText, { InputUnderline } from "./style";
+import LabelContainer from "components/LabelContainer";
+function InputText({
+  label,
+  placeholder = "Please enter here",
+  children,
+  ...rest
+}) {
+  const input = <InputUnderline type="text" placeholder={placeholder} />;
+  return (
+    <StyledInputText {...rest}>
+      {label ? <LabelContainer label={label}>{input}</LabelContainer> : input}
+    </StyledInputText>
+  );
+}
+
+InputText.propTypes = {
+  label: PropTypes.string,
+  placeholder: PropTypes.string,
+};
+
+export default InputText;
