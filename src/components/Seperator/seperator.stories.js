56b1db33dbd41b12eb2e69fdda6e82f0c687d543
ruida shen <shenruida@gmail.com>
2020-11-18 21:13:56 -0800

    Seperator Component

---
+++ src/components/Seperator/seperator.stories.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Seperator from "./";
+
+export default {
+  title: "UI Components/Seperator",
+  component: Seperator,
+};
+
+export const Default = () => {
+  return (
+    <div style={{ padding: "24px" }}>
+      <Seperator></Seperator>
+    </div>
+  );
+};
