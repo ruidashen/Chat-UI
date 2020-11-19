c31bebf869d57ddbcab033ebafde68e009da910a
ruida shen <shenruida@gmail.com>
2020-11-18 23:16:22 -0800

    Label Container

---
+++ src/components/LabelContainer/labelContainer.stories.js
@@ -0,0 +1,11 @@
+import React from "react";
+import LabelContainer from "./";
+
+export default {
+  title: "LabelContainer",
+  component: LabelContainer,
+};
+
+export const Default = () => {
+  return <LabelContainer>Default</LabelContainer>;
+};
