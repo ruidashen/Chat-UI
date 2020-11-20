2eb256492cd5ea90f23b2ec144337a40de020261
ruida shen <shenruida@gmail.com>
2020-11-20 15:14:28 -0800

    BlockList Component

---
+++ src/components/BlockedList/blockedList.stories.js
@@ -0,0 +1,11 @@
+import React from "react";
+import BlockedList from "./";
+
+export default {
+  title: "Page Components/BlockedList",
+  component: BlockedList,
+};
+
+export const Default = () => {
+  return <BlockedList></BlockedList>;
+};
---
+++ src/components/BlockedList/index.js
@@ -0,0 +1,49 @@
+import React from "react";
+import PropTypes from "prop-types";
+import StyledBlockedList, {
+  BlcokedName,
+  BlockedAvatar,
+  CloseableAvatar,
+  CloseIcon,
+  FriendList,
+  SettingsMenu,
+} from "./style";
+import "styled-components/macro";
+import { ReactComponent as ArrowMenuLeft } from "assets/icons/arrowMenuLeft.svg";
+import { ReactComponent as CloseCircle } from "assets/icons/closeCircle.svg";
+import Icon from "components/Icon";
+import Text from "components/Text";
+import face from "assets/images/face-male-1.jpg";
+
+function BlockedList({ children, ...rest }) {
+  return (
+    <StyledBlockedList {...rest}>
+      <SettingsMenu>
+        <Icon
+          icon={ArrowMenuLeft}
+          css={`
+            cursor: pointer;
+          `}
+        ></Icon>
+        <Text size="xxlarge">Blocked Friends</Text>
+      </SettingsMenu>
+      <FriendList>
+        {new Array(8).fill(0).map((_, i) => {
+          return (
+            <CloseableAvatar key={i}>
+              <BlockedAvatar size="105px" src={face}></BlockedAvatar>
+              <CloseIcon width={46} height={51} icon={CloseCircle}></CloseIcon>
+              <BlcokedName>Friend {i + 1}</BlcokedName>
+            </CloseableAvatar>
+          );
+        })}
+      </FriendList>
+    </StyledBlockedList>
+  );
+}
+
+BlockedList.propTypes = {
+  children: PropTypes.any,
+};
+
+export default BlockedList;
