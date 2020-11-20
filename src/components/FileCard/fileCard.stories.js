48bd080f8740a2d4c3af717d002443b8c04b4fa4
ruida shen <shenruida@gmail.com>
2020-11-20 15:52:31 -0800

    FileCard Component

---
+++ src/components/FileCard/fileCard.stories.js
@@ -0,0 +1,11 @@
+import React from "react";
+import FileCard from "./";
+
+export default {
+  title: "UI Components/FileCard",
+  component: FileCard,
+};
+
+export const Default = () => {
+  return <FileCard></FileCard>;
+};
